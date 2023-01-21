602e57691b01e4c377b31e8153e364551cb89803
KingZoldack <jeanpaulismael@gmail.com>
2023-01-21 02:37:01 +0000

    Animations and Jumps
    Create wall slide and wall jump.  Create animations. Fix raycast horizontal line. Add facing direction when in the air (jump).

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,6 +13,11 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D _rb;
     Animator _anim;
 
+    [Header("Component References")]
+    [Tooltip("Set reference to StringReference script.")]
+    [SerializeField]
+    StringReferences _references;
+
     [Header("Movement Variables")]
     [Tooltip("Set the movement speed of the player.")]
     [SerializeField]
@@ -21,23 +27,39 @@ public class PlayerController : MonoBehaviour
     int jumpForce = 1;
     Vector2 _moveDirection;
 
+    [Header("Raycast Configurations")]
+    [Tooltip("Select ground layer.")]
     [SerializeField]
     LayerMask _whatIsGround;
+    [Tooltip("Calculate distance of raycast to ground")]
     [SerializeField]
     float _distanceFromGround;
+    [Tooltip("Calculate distance of raycast to wall")]
+    [SerializeField]
+    float _distanceFromWall;
+    int _facingDirection = 1;
+
+    [Header("For Testing")]
     [SerializeField]
     bool _isGrounded;
+    [SerializeField]
+    bool _isWall;
+
+    //Jump Parametes
     bool _canDoubleJump = false;
+    float _jumpAnimationDelay = 0.03f;
 
     //Animation and State
     string _currentState;
     string _idleAnimation, _runAnimation, _jumpAnimation;
+    string _verticalWallAnimation2;
 
     enum AnimStates
     {
         idle,
         run,
-        jump
+        jump,
+        vertical_wall2
 
     }
 
@@ -72,18 +94,21 @@ public class PlayerController : MonoBehaviour
         _idleAnimation = AnimStates.idle.ToString();
         _runAnimation = AnimStates.run.ToString();
         _jumpAnimation = AnimStates.jump.ToString();
+        _verticalWallAnimation2 = AnimStates.vertical_wall2.ToString();
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - _distanceFromGround, transform.position.z));
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + _distanceFromWall * _facingDirection, transform.position.y, transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
         CollisionChecks();
+        WallHang();
 
         _moveDirection = _move.ReadValue<Vector2>();
     }
@@ -96,22 +121,41 @@ public class PlayerController : MonoBehaviour
     private void CollisionChecks()
     {
         _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, _distanceFromGround, _whatIsGround);
+
+        _isWall = Physics2D.Raycast(transform.position, Vector2.right * _facingDirection, _distanceFromWall, _whatIsGround);
     }
     private void Move()
     {
         _rb.velocity = new Vector2(_moveDirection.x * _moveSpeed, _rb.velocity.y);
 
-        if (_rb.velocity.x != 0 && _rb.velocity.y == 0)
+        if (_rb.velocity.x != 0)
         {
-            ChangeAnimationState(_runAnimation);
+            //Handles animations for in the air and on the ground
+            if (_rb.velocity.y == 0)
+                ChangeAnimationState(_runAnimation);
+            else if (_rb.velocity.y != 0 && !_isWall)
+                ChangeAnimationState(_jumpAnimation);
 
-            if (_rb.velocity.x > 0)
-                transform.localScale = new Vector2(1, 1);
-            else
-                transform.localScale = new Vector2(-1, 1);
+            ChangeDirection();
         }
+
         if (_rb.velocity.x == 0 && _rb.velocity.y == 0)
             ChangeAnimationState(_idleAnimation);
+    }
+
+    private void ChangeDirection()
+    {
+        if (_rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector2(1, 1); //Player face right
+            _facingDirection = 1; //Raycast face right
+        }
+
+        else if (_rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector2(-1, 1); //Player face left
+            _facingDirection = -1; //Raycast face left
+        }
     }
 
     void Jump(InputAction.CallbackContext context)
@@ -126,21 +170,39 @@ public class PlayerController : MonoBehaviour
             else if (_canDoubleJump)
             {
                 _canDoubleJump = false;
+                StartCoroutine(StartJumpAnimationRoutine()); //Replays jump animation
                 Jump();
             }
         }
-        
+    }
+
+    IEnumerator StartJumpAnimationRoutine()
+    {
+        _anim.SetBool(_references.DidDoubleJumpBool, true);
+        yield return new WaitForSeconds(_jumpAnimationDelay);
+        _anim.SetBool(_references.DidDoubleJumpBool, false);
     }
 
     private void Jump()
     {
+        if(!_isWall) //Enables player to jump off walls but only when they face away
         _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
-        _anim.StopPlayback();
-        ChangeAnimationState(_jumpAnimation);
+    }
+
+    private void WallHang()
+    {
+        if (_isWall && !_isGrounded)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * 0.99f);
+            ChangeAnimationState(_verticalWallAnimation2);
+            _canDoubleJump = true;
+        }
     }
 
     public void ChangeAnimationState(string newState)
     {
+        _anim.StopPlayback();
+
         //stop the same animation from interrupting itself
         if (_currentState == newState)
             return;
--- Assets/_Scripts/StringReferences.cs
+++ Assets/_Scripts/StringReferences.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class StringReferences : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static StringReferences Instance { get; private set; }
+    private void Awake()
     {
-        
+        if(Instance == null)
+           Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Animator Params
+    string didDoubleJumpBool = "didDoubleJump";
+
+
+    //Props
+    public string DidDoubleJumpBool { get { return didDoubleJumpBool; } }
 }
